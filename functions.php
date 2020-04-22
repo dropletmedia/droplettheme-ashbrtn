2c72f49d3fcc196d92b85281325b7e232ba5844f
dropletmedia <phil@dropletmedia.co.uk>
2020-04-22 15:06:24 +0100

    lighter version no smoke

--- functions.php
+++ functions.php
@@ -13,16 +13,19 @@ if (!function_exists('theme_enqueue_styles')) {
     function theme_enqueue_styles()
     {
         wp_enqueue_style(
-		'droplettheme',
-		get_stylesheet_directory_uri() . '/style.css',
-		[
-			'hello-elementor'
-		],
-		'1.0.0'
-	    );
-		wp_enqueue_script('three', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r61/three.min.js');
-		
-		wp_enqueue_script(
-			'dropletscript', get_stylesheet_directory_uri() . '/dropletscript.js', array( 'jquery', 'three' ));
+            'droplettheme',
+            get_stylesheet_directory_uri() . '/style.css',
+            [
+            'hello-elementor'
+        ],
+            '1.0.0'
+        );
+        // wp_enqueue_script('three', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r61/three.min.js');
+        
+        // wp_enqueue_script(
+        //     'dropletscript',
+        //     get_stylesheet_directory_uri() . '/dropletscript.js',
+        //     array( 'jquery', 'three' )
+        // );
     }
 }
